e41c90a4790c61d7696b81a507344bbccd645a19
keonoh00 <keonoh00@gmail.com>
2021-10-10 14:37:15 +0200

    Modified 2 and Updated 20_ValidParenthesis

---
+++ 20_ValidParenthesis.py
@@ -0,0 +1,63 @@
+class Solution:
+  def isValid(self, s: str) -> bool:
+    if len(s)%2 != 0:
+      return False
+
+    pair = {
+      "(" : ")",
+      "{" : "}",
+      "[" : "]",
+      ")" : "Prevent Key Error",
+      "}" : "Prevent Key Error",
+      "]" : "Prevent Key Error",
+    }
+
+    i = 0
+    while len(s)-1 > i:
+      if pair[s[i]] == s[i+1]:
+        s = s[:i] + s[i+2:]
+        i = 0
+      else:
+        i += 1
+    
+    if len(s) > 0:
+      return False
+    return True
+
+sol = Solution()
+print(sol.isValid("()"))
+print(sol.isValid("()[]{}"))
+print(sol.isValid("(]"))
+print(sol.isValid("([)]"))
+print(sol.isValid("{[]}"))
+
+"""
+Example 1:
+
+Input: s = "()"
+Output: true
+
+
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+
+
+Example 3:
+
+Input: s = "(]"
+Output: false
+
+
+Example 4:
+
+Input: s = "([)]"
+Output: false
+
+
+Example 5:
+
+Input: s = "{[]}"
+Output: true
+"""
