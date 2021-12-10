090a675c69b228c84768cbbad1fad8482ce17245
keonoh00 <keonoh00@gmail.com>
2021-12-10 21:55:09 +0100

    Create 5_LongestPalindromicSubstring.py
    Brute Force Approach

---
+++ 5_LongestPalindromicSubstring.py
@@ -0,0 +1,53 @@
+# Given a string s, return the longest palindromic substring in s.
+
+# Brute Force Approach
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        diction = dict()
+        if len(s) == 1:
+            return s
+        for i in range(len(s), 0, -1):
+            for j in range(len(s)-i+1):
+                substring = s[j:j+i]
+                if substring == substring[::-1]:
+                    return substring
+
+
+check = Solution()
+print(check.longestPalindrome("babad"))
+print(check.longestPalindrome("cbbd"))
+print(check.longestPalindrome("a"))
+print(check.longestPalindrome("ac"))
+print(check.longestPalindrome("bb"))
+
+"""
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Note: "aba" is also a valid answer.
+
+
+Example 2:
+
+Input: s = "cbbd"
+Output: "bb"
+
+
+Example 3:
+
+Input: s = "a"
+Output: "a"
+
+
+Example 4:
+
+Input: s = "ac"
+Output: "a"
+
+
+Example 5:
+
+Input: s = "bb"
+Output: "bb"
+"""
